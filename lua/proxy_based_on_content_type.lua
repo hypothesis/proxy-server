4645e4e6dbf024746f041b2cdab7ab98a4687cb1
Hannah Stepanek <hmstepanek@hypothes.is>
2019-10-21 17:38:04 -0700

    Try-except failed HEAD requests
    Previously if a HEAD request resulted in an erroneous http status code,
    the whole originating request would fail inside nginx. Now the failure will
    be caught and a GET request will be issued instead.

--- lua/proxy_based_on_content_type.lua
+++ lua/proxy_based_on_content_type.lua
@@ -1,6 +1,9 @@
-local response = ngx.location.capture("/id_/follow_redirect" .. ngx.var.request_uri, { method = ngx.HTTP_HEAD})
-if (response.header["Content-Type"] == nil) then
+local success, response = pcall(ngx.location.capture("/id_/follow_redirect" .. ngx.var.request_uri, { method = ngx.HTTP_HEAD}))
+if (success and response.header["Content-Type"] == nil) then
     ngx.log(ngx.STDERR, "HEAD request for " .. ngx.var.request_uri .. " didn't have Content-Type header.")
+    response = ngx.location.capture("/id_/follow_redirect" .. ngx.var.request_uri)
+elseif not success then
+    ngx.log(ngx.STDERR, "HEAD request for " .. ngx.var.request_uri .. " failed.")
     response = ngx.location.capture("/id_/follow_redirect" .. ngx.var.request_uri)
 end
 
