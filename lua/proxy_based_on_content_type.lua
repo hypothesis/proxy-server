4bb880e84330f507f3fe4813b8c823a957324c2c
Hannah Stepanek <hmstepanek@hypothes.is>
2019-10-23 17:07:57 -0700

    Fix bug where google pdf urls redirect to via
    Previously google pdf urls redirected to via for several reasons:
    1. The /id_/follow_redirect endpoint was missing a recursive call to
       follow multiple redirects rather than just one.
    2. Google pdf urls do not accept HEAD requests.
    3. ngx.location.capture does not appear to support this multi-redirect
       use case so switch to using lua-http to create subrequests.
    4. The DNS was resolving the google pdf domain to an unsupported ipv6 address.
    
    Move resolver to http block and turn ipv6 off.

--- lua/proxy_based_on_content_type.lua
+++ lua/proxy_based_on_content_type.lua
@@ -1,14 +1,33 @@
-local success, response = pcall(ngx.location.capture("/id_/follow_redirect" .. ngx.var.request_uri, { method = ngx.HTTP_HEAD}))
-if (success and response.header["Content-Type"] == nil) then
-    ngx.log(ngx.STDERR, "HEAD request for " .. ngx.var.request_uri .. " didn't have Content-Type header.")
-    response = ngx.location.capture("/id_/follow_redirect" .. ngx.var.request_uri)
-elseif not success then
-    ngx.log(ngx.STDERR, "HEAD request for " .. ngx.var.request_uri .. " failed.")
-    response = ngx.location.capture("/id_/follow_redirect" .. ngx.var.request_uri)
+local http = require "resty.http"
+local httpc = http.new()
+--httpc:set_timeout(4000)
+httpc:connect("127.0.0.1", 9081)
+
+function request_content_type_header(method)
+    local response, err = httpc:request({
+      path = "/id_/follow_redirect" .. ngx.var.request_uri, 
+      method = method,
+      headers = ngx.req.get_headers(),
+      keepalive_timeout = 60,
+      keepalive_pool = 10,
+    })
+    if response and response.status then
+        ngx.log(ngx.STDERR, method .. " request for " .. ngx.var.request_uri .. " responded with: " .. response.status)
+    end
+    return response, err
 end
 
-if (response.header["Content-Type"] == "application/pdf" 
-        or response.header["Content-Type"] == "application/x-pdf") then
+local response, err = request_content_type_header("HEAD")
+local success = response and response.status >= 200 and response.status < 400
+
+if not success then
+    ngx.log(ngx.STDERR, "HEAD request for " .. ngx.var.request_uri .. " failed.")
+    response, err = request_content_type_header("GET")
+end
+
+ngx.log(ngx.STDERR, "Using Content-Type: " .. response.headers["Content-Type"] .. " to direct request")
+if (response.headers["Content-Type"] == "application/pdf" 
+        or response.headers["Content-Type"] == "application/x-pdf") then
     return ngx.exec("/pdf" .. ngx.var.request_uri)
 else
     return ngx.redirect(os.getenv("VIA_URL") .. ngx.var.request_uri, 302)
