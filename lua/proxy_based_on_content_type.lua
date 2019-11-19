635034f6753d4adff772b4fe0b20b4a95325e2c3
Hannah Stepanek <hmstepanek@hypothes.is>
2019-11-18 18:56:46 -0800

    Add trailing / to proxied url's with empty paths
    Previously if the url did not have a path specified such as:
        http://example.com
    
    It would come back with a 404 response. This has to do with the way
    nginx interprets proxy_pass url's with no paths specified. It treats
    them as a proxy server and appends the path from the location to the
    server. It is our intention in all cases that the proxied url be the full
    server + path as opposed to just the server so in cases with very simple
    urls we must indicate this to nginx by adding the trailing /.

--- lua/proxy_based_on_content_type.lua
+++ lua/proxy_based_on_content_type.lua
@@ -2,16 +2,22 @@ local http = require "resty.http"
 local httpc = http.new()
 httpc:connect("127.0.0.1", 9081)
 
+-- parse_uri sets path = '/' for empty paths which is necessary to signal nginx to
+-- treat the proxy_pass uri as the server + path as opposed to just the server.
+-- See docs http://nginx.org/en/docs/http/ngx_http_proxy_module.html#proxy_pass for details.
+local scheme, host, port, path = unpack(http:parse_uri(ngx.var.request_uri:sub(2)))
+local request_uri = "/" .. scheme .. '://' .. host .. ":" .. port .. path
+
 function request_content_type_header(method)
     local response, err = httpc:request({
-      path = "/id_/follow_redirect" .. ngx.var.request_uri, 
+      path = "/id_/follow_redirect" .. request_uri, 
       method = method,
       headers = ngx.req.get_headers(),
       keepalive_timeout = 60,
       keepalive_pool = 10,
     })
     if response and response.status then
-        ngx.log(ngx.STDERR, method .. " request for " .. ngx.var.request_uri .. " responded with: " .. response.status)
+        ngx.log(ngx.STDERR, method .. " request for " .. request_uri .. " responded with: " .. response.status)
     end
     return response, err
 end
@@ -20,7 +26,7 @@ local response, err = request_content_type_header("HEAD")
 local success = response and response.status >= 200 and response.status < 400
 
 if not success then
-    ngx.log(ngx.STDERR, "HEAD request for " .. ngx.var.request_uri .. " failed.")
+    ngx.log(ngx.STDERR, "HEAD request for " .. request_uri .. " failed.")
     response, err = request_content_type_header("GET")
 end
 
@@ -28,7 +34,7 @@ local content_type_header = response.headers["Content-Type"] or "nil"
 ngx.log(ngx.STDERR, "Using Content-Type: " .. content_type_header .. " to direct request")
 if (content_type_header == "application/pdf"
         or content_type_header == "application/x-pdf") then
-    return ngx.exec("/pdf" .. ngx.var.request_uri)
+    return ngx.exec("/pdf" .. request_uri)
 else
     return ngx.redirect(os.getenv("VIA_URL") .. ngx.var.request_uri, 302)
 end
